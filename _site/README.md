ccd7c3db795479e2812eace8d0afaae3f4ce61dd
raulghm <raulghm@gmail.com>
2018-04-28 10:20:05 -0300

    fix(site): remove site

--- _site/README.md
+++
@@ -1,3 +0,0 @@
-# BeerJS Site
-
-WIP
--- _site/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './components/App'
-
-new Vue({
-  el: '#root',
-  components: { App },
-  template: '<App/>',
-  render: h => h(App),
-})
